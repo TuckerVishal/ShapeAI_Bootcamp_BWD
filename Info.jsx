53dbc7a7254fd5834478d74ce2fbe588c979f4d7
TuckerVishal <86250846+TuckerVishal@users.noreply.github.com>
2021-06-26 17:02:00 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>7 day free Basic Web Development Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
